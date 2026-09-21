44846be5525ad99cc2295f27ecd90f3679c349eb
acecrum <axertlie@gmail.com>
2026-09-21 02:16:10 -0400

    sdg

--- SpoofedName/SpoofPlugin.cs
+++ SpoofedName/SpoofPlugin.cs
@@ -7,7 +7,7 @@ namespace SpoofedName;
 public class SpoofPlugin : Plugin
 {
     public override string Name { get; } = "Spoofed Name";
-    public override string Description { get; } = "Staff can change their name on everyones clients without having to change their Steam name";
+    public override string Description { get; } = "Staff can change their name without having to change their Steam name";
     public override string Author { get; } = "acecrum";
     public override Version Version { get; } = new Version(1, 0, 0);
     public override Version RequiredApiVersion { get; } = new (LabApiProperties.CompiledVersion);
