9623da3db3d24cdb9007b6b24d3cbeb006e731c9
acecrum <axertlie@gmail.com>
2026-09-21 15:43:48 -0400

    typo fix and support for spaces in name with string.Join

--- SpoofedName/SpoofCommand.cs
+++ SpoofedName/SpoofCommand.cs
@@ -8,7 +8,7 @@ public class SpoofCommand : ICommand, IUsageProvider
 {
     public string Command => "spoof";
     public string[] Aliases => [];
-    public string Description => "Spoofs the the executor's username";
+    public string Description => "Spoofs the executor's username";
     public string[] Usage => ["Name"];
 
     public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
@@ -25,19 +25,15 @@ public class SpoofCommand : ICommand, IUsageProvider
         {
             case 0:
                 if (cSender?.UserId != null) _jsonSaving?.Delete(cSender.UserId);
-
                 response = "Spoof removed, rejoin for the name change to occur.";
                 return true;
-            case > 1:
-                response = "Only one argument is allowed";
-                return false;
         }
-        var spoofName = arguments.ElementAt(0);
+        var spoofName = string.Join(" ", arguments.Skip(0));
 
         cSender?.ReferenceHub.nicknameSync.MyNick = spoofName;
 
         if (cSender != null) _jsonSaving?.Save(cSender, spoofName);
-
+        
         response = $"Spoofed to {spoofName}, rejoin for the name change to occur.";
         return true;
     }
